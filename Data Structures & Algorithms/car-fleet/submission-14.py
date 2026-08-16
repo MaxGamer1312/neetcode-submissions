eaaf021e7806c03283f63c01ad4ea36d7c39bbce
Victor Sanchez <vsanchez2028@utexas.edu>
2026-08-16 15:12:54 -0500

    Add: car-fleet - submission-14

---
+++ Data Structures & Algorithms/car-fleet/submission-14.py
@@ -0,0 +1,13 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        unique_fleets = 0
+        for i in range(len(position)):
+            position[i] = (position[i], (target-position[i])/speed[i])
+        position.sort(key = lambda x: x[0], reverse = True)
+        
+        prev_time = -1
+        for i in range(len(position)):
+            if prev_time == -1 or position[i][1] > prev_time:
+                prev_time = position[i][1]
+                unique_fleets += 1
+        return unique_fleets
