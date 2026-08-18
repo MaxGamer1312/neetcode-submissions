50c0c0248b6372abb93d9f969691a43bd77a03b4
Victor Sanchez <vsanchez2028@utexas.edu>
2026-08-18 17:48:18 -0500

    Add: time-based-key-value-store - submission-8

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-8.py
@@ -0,0 +1,24 @@
+import bisect
+class TimeMap:
+
+    def __init__(self):
+        self.data = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        bisect.insort(self.data[key], [value, timestamp], key = lambda x: x[1])
+
+    def get(self, key: str, timestamp: int) -> str:
+        i = 0
+        j = len(self.data[key]) - 1
+        while i <= j:
+            middle_index = (i + j) // 2
+            middle_element = self.data[key][middle_index]
+            if middle_element[1] == timestamp:
+                return middle_element[0]
+            if middle_element[1] < timestamp:
+                i = middle_index + 1
+            else:
+                j = middle_index - 1
+        if j < 0:
+            return ""
+        return self.data[key][j][0]
